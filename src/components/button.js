a3b9d9b7d1ce3e8c2e9d5e71e05062cb70d93ebd
Addison <addison@framer.com>
2019-06-16 13:41:25 +0200

    Updates

--- src/components/button.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Button = props => (
-  <a href={props.href} target="_blank" rel="noopener noreferrer" id="button">
-    <div>{props.text}</div>
-  </a>
-)
-
-export default Button
--- src/components/footer.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-const Footer = () => <div className="footer" />
-
-export default Footer
