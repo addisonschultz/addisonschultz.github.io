430eafa13edb62f86e29ee28dc53abe90ab1b992
Addison <addison@framer.com>
2020-03-13 22:21:42 +0100

    Auto-generated commit from GH Pages

--- component---src-pages-404-js-33371569f193547bd877.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("Bl7J"),u=t("vrFN");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("div",null,l.a.createElement("h1",null,"nope")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-33371569f193547bd877.js.map
